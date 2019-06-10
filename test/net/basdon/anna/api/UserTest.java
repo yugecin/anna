3189f739ac284d6ef0dc78e4dd7c201f7d8c48c7
yugecin <yugecin@users.noreply.github.com>
2019-06-10 15:30:02 +0200

    test user matching

--- test/net/basdon/anna/api/UserTest.java
+++ test/net/basdon/anna/api/UserTest.java
@@ -4,7 +4,7 @@ package net.basdon.anna.api;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class UserTest
 {
@@ -13,6 +13,7 @@ public
 void test_parse_normal()
 {
 	User u = User.parse("--robin!nick@host--".toCharArray(), 2, 17);
+
 	assertEquals("robin", new String(u.nick));
 	assertEquals("nick", new String(u.name));
 	assertEquals("host", new String(u.host));
@@ -23,6 +24,7 @@ public
 void test_parse_nonick()
 {
 	User u = User.parse("--robin@host--".toCharArray(), 2, 12);
+
 	assertEquals("robin", new String(u.nick));
 	assertEquals(null, u.name);
 	assertEquals("host", new String(u.host));
@@ -33,6 +35,7 @@ public
 void test_parse_invalid_exat()
 {
 	User u = User.parse("--robin!@host--".toCharArray(), 2, 13);
+
 	assertEquals(null, u);
 }
 
@@ -41,6 +44,58 @@ public
 void test_parse_invalid_nohost()
 {
 	User u = User.parse("--robin!nick@--".toCharArray(), 2, 13);
+
 	assertEquals(null, u);
+}
+
+@Test
+public
+void test_match_full()
+{
+	User u1 = User.parse("--nick!name@host--".toCharArray(), 2, 16);
+	User u2 = User.parse("--n_ck!name@host--".toCharArray(), 2, 16);
+	User u3 = User.parse("--nick!n_me@host--".toCharArray(), 2, 16);
+	User u4 = User.parse("--nick!name@h_st--".toCharArray(), 2, 16);
+	User u5 = User.parse("--nick!name@host--".toCharArray(), 2, 16);
+
+	assertTrue(!u1.matches(u2));
+	assertTrue(!u1.matches(u3));
+	assertTrue(!u1.matches(u4));
+	assertTrue(u1.matches(u5));
+}
+
+@Test
+public
+void test_match_wildcards()
+{
+	User u1 = User.parse("--nick!name@host--".toCharArray(), 2, 16);
+	User u2 = User.parse("--n_ck!name@host--".toCharArray(), 2, 16);
+	User u3 = User.parse("--nick!n_me@host--".toCharArray(), 2, 16);
+	User u4 = User.parse("--nick!name@h_st--".toCharArray(), 2, 16);
+
+	User u5 = User.parse("--*!name@host--".toCharArray(), 2, 13);
+	User u6 = User.parse("--nick!*@host--".toCharArray(), 2, 13);
+	User u7 = User.parse("--nick!name@*--".toCharArray(), 2, 13);
+	User u8 = User.parse("--*!*@*--".toCharArray(), 2, 7);
+
+	assertTrue(u5.matches(u1));
+	assertTrue(u5.matches(u2));
+	assertTrue(!u5.matches(u3));
+	assertTrue(!u1.matches(u4));
+
+	assertTrue(u6.matches(u1));
+	assertTrue(!u6.matches(u2));
+	assertTrue(u6.matches(u3));
+	assertTrue(!u6.matches(u4));
+
+	assertTrue(u7.matches(u1));
+	assertTrue(!u7.matches(u2));
+	assertTrue(!u7.matches(u3));
+	assertTrue(u7.matches(u4));
+
+	assertTrue(u8.matches(u1));
+	assertTrue(u8.matches(u2));
+	assertTrue(u8.matches(u3));
+	assertTrue(u8.matches(u4));
 }
 }
